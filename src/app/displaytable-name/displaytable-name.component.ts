557610ecff01e1f27f0a811a954c000e713af47b
Subbulakshmi-4 <121540421+Subbulakshmi-4@users.noreply.github.com>
2023-10-13 13:06:32 +0530

    Pushing Displaychanges and styles

--- src/app/displaytable-name/displaytable-name.component.ts
+++ src/app/displaytable-name/displaytable-name.component.ts
@@ -13,6 +13,12 @@ export class DisplaytableNameComponent implements OnInit {
   tableNames: EntityListDto[] = [];
   errorMessage: string = '';
 
+  pagedData: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 5; // Number of items per page
+
+  searchText = ''; // Variable to store user input for search
+
   constructor(private entitylistService: EntitylistService, private router: Router) {} // Inject Router
 
   ngOnInit(): void {
@@ -25,7 +31,46 @@ export class DisplaytableNameComponent implements OnInit {
         this.errorMessage = 'No Data Available';  // Update error message
       }
     );
+
+    this.setPage(this.currentPage); // Initialize the first page
   }
+
+  setPage(page: number) {
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = Math.min(startIndex + this.itemsPerPage, this.tableNames.length);
+    this.pagedData = this.tableNames.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.setPage(this.currentPage);
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.setPage(this.currentPage);
+    }
+  }
+
+  onSearch() {
+    // Filter the data based on the searchText
+    if (this.searchText) {
+      this.pagedData = this.tableNames.filter(item =>
+        item.entityName.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+    } else {
+      // If search text is empty, reset to the original data
+      this.setPage(this.currentPage);
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.tableNames.length / this.itemsPerPage);
+  }
+  
   createTable() {
     // ... Your existing createTable code ...
   
